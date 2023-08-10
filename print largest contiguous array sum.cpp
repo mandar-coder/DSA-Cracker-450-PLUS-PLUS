1a8e47d53c090399b42ff723429749fcfbd36588
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 15:00:53 +0530

    Day 12

--- print largest contiguous array sum.cpp
+++ print largest contiguous array sum.cpp
@@ -1,30 +1,31 @@
-//print largest contiguous array sum
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int a[], int size)
-{
-	int max_so_far = INT_MIN, max_ending_here = 0;
+// Function to find the maximum contiguous subarray sum
+int maxSubArraySum(int a[], int size) {
+    int max_so_far = INT_MIN, max_ending_here = 0;
 
-	for (int i = 0; i < size; i++) {
-		max_ending_here = max_ending_here + a[i];
-		if (max_so_far < max_ending_here)
-			max_so_far = max_ending_here;
+    for (int i = 0; i < size; i++) {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
 
-		if (max_ending_here < 0)
-			max_ending_here = 0;
-	}
-	return max_so_far;
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
 }
 
-int main()
-{
-	int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-	int n = sizeof(a) / sizeof(a[0]);
+int main() {
+    // Program to print the largest contiguous array sum
+    int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+    int n = sizeof(a) / sizeof(a[0]);
 
+    // Call the maxSubArraySum function to find and display the maximum contiguous sum
+    int max_sum = maxSubArraySum(a, n);
+    cout << "Maximum contiguous sum is " << max_sum;
 
-	int max_sum = maxSubArraySum(a, n);
-	cout << "Maximum contiguous sum is " << max_sum;
-	return 0;
+    return 0;
 }
 
+// Added by Mandar - August 2023
