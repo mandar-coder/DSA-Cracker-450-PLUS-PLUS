1bd3aad789777bc7dc46a471d5fe3411dc5ffe2e
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 13:47:31 +0530

    Day 3

--- find_hcf.cpp
+++ find_hcf.cpp
@@ -1,25 +1,31 @@
 #include<iostream>
-using namespace std;
-//find hcf
-int main(){
-    int a, b, small=1, hfc;
-    cin>>a;
-    cin>>b;
+
+// Function to find the highest common factor (HCF)
+int main() {
+    int a, b, small = 1, hfc;
+
+    // Get input for two numbers
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
     
-    if(a<b){
-        small=a;
-    }
-    else{
-        small=b;
+    // Determine the smaller of the two numbers
+    if (a < b) {
+        small = a;
+    } else {
+        small = b;
     }
     
-    for(int i=1; i<small+1; i++){
-        if(a % i == 0 && b % i == 0){
+    // Find the HCF using a loop
+    for (int i = 1; i < small + 1; i++) {
+        if (a % i == 0 && b % i == 0) {
             hfc = i;
         }
     }
     
-    cout<<hfc<<endl;
-    
+    // Display the HCF
+    cout << "HCF of " << a << " and " << b << " is: " << hfc << endl;
+
     return 0;
 }
+
+// Added by Mandar - August 2023
