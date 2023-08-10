2e6c5e8c24f64e9e48ae2d38432db30b889f52eb
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:37:05 +0530

    Day 9

--- Move all negative numbers to beginning and positive to end.cpp
+++ Move all negative numbers to beginning and positive to end.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void move(vector<int>& arr){
-sort(arr.begin(),arr.end());
+// Function to move negative numbers to the beginning of the array
+void move(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
 }
 
 int main() {
-
-	vector<int> arr = { -1, 22, -31, 4, 55, 6, -71, 8, 93 };
-	move(arr);
-	for (int e : arr)
-	cout<<e << " ";
-	return 0;
+    // Program to move negative numbers to the beginning of the array
+    vector<int> arr = { -1, 22, -31, 4, 55, 6, -71, 8, 93 };
+    
+    // Call the move function to sort the array
+    move(arr);
+    
+    // Display the sorted array
+    for (int e : arr)
+        cout << e << " ";
+    
+    return 0;
 }
+
+// Added by Mandar - August 2023
+
