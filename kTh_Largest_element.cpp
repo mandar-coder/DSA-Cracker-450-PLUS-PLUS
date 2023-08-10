e4a62efdfd5cca87373ecd09723190615b30fda3
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:13:31 +0530

    Day 7

--- kTh_Largest_element.cpp
+++ kTh_Largest_element.cpp
@@ -1,25 +1,31 @@
-//kth largest element
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int kth(int arr[],int k,int n){
+// Function to find the kth largest element
+int kthLargest(int arr[], int k, int n) {
+    // Sort the array in ascending order
+    sort(arr, arr + n);
     
-    //sort array
-    sort(arr, arr+n);
-    
-    //print kth no
-    cout<<arr[k-1];
+    // Print the kth largest element
+    cout << "The " << k << "th largest element is: " << arr[n - k] << endl;
     return 0;
 }
 
 int main() {
+    // Program to find the kth largest element
     int k;
-    int arr[]={1,9,8,4,6};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<"No:"<<endl;
-    cin>>k;
-    
-    kth(arr,k, n);
+    int arr[] = {1, 9, 8, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Get the value of k from the user
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+    // Call the kthLargest function to find and display the kth largest element
+    kthLargest(arr, k, n);
+
     return 0;
 }
+
+// Added by Mandar - August 2023
