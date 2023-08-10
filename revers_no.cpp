db20024ba47d4c3eff0ed9628f920ffb0ae36f04
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:11:48 +0530

    Day 6

--- revers_no.cpp
+++ revers_no.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
 using namespace std;
-//reversing logic 
-int reversarray(int arr[], int start, int end){
-    while(start<end){
+
+// Function to reverse an array or string
+int reverseArray(int arr[], int start, int end) {
+    while (start < end) {
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
         start++;
         end--;
     }
+    return 0;
 }
-//print array
-int printarray(int arr[], int n){
-    for(int i=0; i<n;i++){
-        cout<<arr[i]<<" ";
+
+// Function to print the array
+int printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
 
 int main() {
-    //program to reverse an array or string
-    int arr[]={1,2,3,4,5,6};
+    // Program to reverse an array
+    int arr[] = {1, 2, 3, 4, 5, 6};
     
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printarray(arr, n);
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Display the original array
+    cout << "Original Array: ";
+    printArray(arr, n);
     
-    reversarray(arr,0,n-1);
-    printarray(arr, n);
+    // Reverse the array
+    reverseArray(arr, 0, n - 1);
+
+    // Display the reversed array
+    cout << "Reversed Array: ";
+    printArray(arr, n);
+    
     return 0;
 }
+
+// Added by Mandar - August 2023
