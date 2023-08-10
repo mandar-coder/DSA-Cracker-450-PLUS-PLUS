d73c58d63777cf426e5682438655dc437384b098
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:38:14 +0530

    Day 10

--- Union and Intersection of two sorted arrays.cpp
+++ Union and Intersection of two sorted arrays.cpp
@@ -1,40 +1,41 @@
-// two sorted arrays union
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void printUnion(int arr1[], int arr2[], int m, int n)
-{
-	int i = 0, j = 0;
-	while (i < m && j < n) {
-		if (arr1[i] < arr2[j])
-			cout << arr1[i++] << " ";
-
-		else if (arr2[j] < arr1[i])
-			cout << arr2[j++] << " ";
-
-		else {
-			cout << arr2[j++] << " ";
-			i++;
-		}
-	}
-	while (i < m)
-		cout << arr1[i++] << " ";
-
-	while (j < n)
-		cout << arr2[j++] << " ";
+// Function to print the union of two sorted arrays
+void printUnion(int arr1[], int arr2[], int m, int n) {
+    int i = 0, j = 0;
+    
+    while (i < m && j < n) {
+        if (arr1[i] < arr2[j])
+            cout << arr1[i++] << " ";
+        else if (arr2[j] < arr1[i])
+            cout << arr2[j++] << " ";
+        else {
+            cout << arr2[j++] << " ";
+            i++;
+        }
+    }
+    
+    while (i < m)
+        cout << arr1[i++] << " ";
+    
+    while (j < n)
+        cout << arr2[j++] << " ";
 }
 
-/* Driver program to test above function */
-int main()
-{
-	int arr1[] = { 1, 2, 4, 5, 6 };
-	int arr2[] = { 2, 3, 5, 7 };
+int main() {
+    // Program to print the union of two sorted arrays
+    int arr1[] = { 1, 2, 4, 5, 6 };
+    int arr2[] = { 2, 3, 5, 7 };
 
-	int m = sizeof(arr1) / sizeof(arr1[0]);
-	int n = sizeof(arr2) / sizeof(arr2[0]);
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+    int n = sizeof(arr2) / sizeof(arr2[0]);
 
-	printUnion(arr1, arr2, m, n);
+    // Call the printUnion function to print the union
+    printUnion(arr1, arr2, m, n);
 
-	return 0;
+    return 0;
 }
+
+// Added by Mandar - August 2023
+
