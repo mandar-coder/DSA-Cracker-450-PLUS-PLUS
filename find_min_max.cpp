eed9d67fe4e86245dbdefcfe8cc4fe64dea2d136
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:10:43 +0530

    Day 5

--- find_min_max.cpp
+++ find_min_max.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 using namespace std;
-//find min max
-void minmax(int arr[],int n){
-    int max ,min;
-    min= arr[0];
-    max= arr[0];
-    for(int i=0; i<n; i++){
-        if(max < arr[i]){
-            max = arr[i];
+
+// Function to find and display the minimum and maximum values in an array
+void minmax(int arr[], int n) {
+    int max_value, min_value;
+    min_value = arr[0];
+    max_value = arr[0];
+
+    // Iterate through the array to find min and max values
+    for (int i = 0; i < n; i++) {
+        if (max_value < arr[i]) {
+            max_value = arr[i];
         }
-        if(min > arr[i]){
-            min = arr[i];
+        if (min_value > arr[i]) {
+            min_value = arr[i];
         }
     }
-    cout<<max<<endl;
-    cout<<min<<endl;
+
+    // Display the maximum and minimum values
+    cout << "Maximum value: " << max_value << endl;
+    cout << "Minimum value: " << min_value << endl;
 }
 
-int main()
-{
+int main() {
     int n;
-    cout<<"no of element array: "<<endl;
-    cin>>n;
+
+    // Get the number of elements in the array
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
     int arr[n];
-    cout<<"Enter no:"<<endl;
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+
+    // Get input for the array elements
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-  
+
+    // Call the minmax function to find and display min and max values
     minmax(arr, n);
+
     return 0;
 }
+
+// Added by Mandar - August 2023
