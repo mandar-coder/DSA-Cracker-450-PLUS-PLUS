8e490f56b61f5fecbe22ae5c95c24d773fff4b58
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 13:45:53 +0530

    Day 2

--- find_factorial.cpp
+++ find_factorial.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
-//find factorial 
-int factorial(int n){
-    if(n<1){
+
+// Function to find factorial
+int factorial(int n) {
+    if (n < 1) {
         return 1;
-    }else{
-        return n*factorial(n-1);
+    } else {
+        return n * factorial(n - 1);
     }
     return 0;
 }
-int main(){
+
+int main() {
     int n;
+
+    // Get input from the user
+    std::cout << "Enter a number: ";
     std::cin >> n;
-    std::cout<<factorial(n);
+
+    // Calculate and display the factorial
+    std::cout << "Factorial of " << n << " is: " << factorial(n);
+
     return 0;
 }
+
+// Added by Mandar - August 2023
