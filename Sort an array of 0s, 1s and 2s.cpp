c8ce12b0587e19a32c8f62095b75ac8403b8b742
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-10 14:21:09 +0530

    Day 8

--- Sort an array of 0s, 1s and 2s.cpp
+++ Sort an array of 0s, 1s and 2s.cpp
@@ -1,36 +1,55 @@
-// Sort an array of 0s, 1s and 2s
 #include <iostream>
 using namespace std;
 
-void sort123(int arr[],int n){
-    int low=0;
-    int mid=0;
-    int hih=n-1;
-    while(mid <= hih){
-      switch(arr[mid]){
-        case 0 :
-          swap(arr[low++],arr[mid++]);
-          break;
-        case 1:
-          mid++;
-          break;
-        case 2:
-            swap(arr[mid],arr[hih--]);
-            break;
-      } 
+// Function to sort an array of 0s, 1s, and 2s
+void sort123(int arr[], int n) {
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+    
+    while (mid <= high) {
+        switch (arr[mid]) {
+            case 0:
+                swap(arr[low++], arr[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[high--]);
+                break;
+        }
     }
 }
 
-void printarray(int arr[], int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+    cout << endl;
 }
 
 int main() {
-    int arr[]={1,0,2,2,1,0,1,0,0};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    sort123(arr,n);
-    printarray(arr, n);
+    // Program to sort an array of 0s, 1s, and 2s
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter the elements (0, 1, or 2): ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Call the sort123 function to sort the array
+    sort123(arr, n);
+
+    // Call the printArray function to display the sorted array
+    cout << "Sorted Array: ";
+    printArray(arr, n);
+
     return 0;
 }
+
+// Added by Mandar - August 2023
